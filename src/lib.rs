445b025e51dab459147443ea5221da2857b3579d
toast <58362380+Traumatism@users.noreply.github.com>
2022-11-02 17:29:02 +0100

    Fixed lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
-pub struct Memoire<I: Hash + Eq + Clone, O: Clone> {
+pub struct Memoire<I: std::hash::Hash + Eq + Clone, O: Clone> {
     func: fn(I, &mut Memoire<I, O>) -> O,
     cache: HashMap<I, O>,
 }
 
-impl<I: Hash + Eq + Clone, O: Clone> Memoire<I, O> {
+impl<I: std::hash::Hash + Eq + Clone, O: Clone> Memoire<I, O> {
     pub fn new(func: fn(I, &mut Memoire<I, O>) -> O) -> Self {
         Self {
             func,
